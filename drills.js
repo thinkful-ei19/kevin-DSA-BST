0ba60cdbe0341684c393d465e6fe0dd9636cc871
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-22 09:13:27 -0700

    created a tree

--- drills.js
+++ drills.js
@@ -108,5 +108,23 @@ class BinarySearchTree {
     }
   }
 
-
 }
+
+const bst = new BinarySearchTree();
+
+function main() {
+
+  bst.insert(3,3);
+  bst.insert(1,1);
+  bst.insert(4,4);
+  bst.insert(6,6);
+  bst.insert(9,9);
+  bst.insert(2,2);
+  bst.insert(5,5);
+  bst.insert(7,7);
+
+  console.log(bst);
+  return bst;
+}
+
+main();
